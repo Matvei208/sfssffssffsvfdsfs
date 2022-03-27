76c92a6b529dc1aef5f540e11091ad37939ec8fc
Matvei208 <51878189+Matvei208@users.noreply.github.com>
2022-03-27 14:03:21 +0700

    Add files via upload

---
+++ Memory.cpp
@@ -0,0 +1,166 @@
+#include <array>
+#include <cstring>
+#include <string_view>
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <Psapi.h>
+#elif __linux__
+#include <fcntl.h>
+#include <link.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#endif
+
+#include "Interfaces.h"
+#include "Memory.h"
+#include "SDK/LocalPlayer.h"
+
+template <typename T>
+static constexpr auto relativeToAbsolute(uintptr_t address) noexcept
+{
+    return (T)(address + 4 + *reinterpret_cast<std::int32_t*>(address));
+}
+
+static std::pair<void*, std::size_t> getModuleInformation(const char* name) noexcept
+{
+#ifdef _WIN32
+    if (HMODULE handle = GetModuleHandleA(name)) {
+        if (MODULEINFO moduleInfo; GetModuleInformation(GetCurrentProcess(), handle, &moduleInfo, sizeof(moduleInfo)))
+            return std::make_pair(moduleInfo.lpBaseOfDll, moduleInfo.SizeOfImage);
+    }
+    return {};
+#elif __linux__
+    struct ModuleInfo {
+        const char* name;
+        void* base = nullptr;
+        std::size_t size = 0;
+    } moduleInfo;
+
+    moduleInfo.name = name;
+
+    dl_iterate_phdr([](struct dl_phdr_info* info, std::size_t, void* data) {
+        const auto moduleInfo = reinterpret_cast<ModuleInfo*>(data);
+        if (!std::string_view{ info->dlpi_name }.ends_with(moduleInfo->name))
+            return 0;
+
+        if (const auto fd = open(info->dlpi_name, O_RDONLY); fd >= 0) {
+            if (struct stat st; fstat(fd, &st) == 0) {
+                if (const auto map = mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0); map != MAP_FAILED) {
+                    const auto ehdr = (ElfW(Ehdr)*)map;
+                    const auto shdrs = (ElfW(Shdr)*)(std::uintptr_t(ehdr) + ehdr->e_shoff);
+                    const auto strTab = (const char*)(std::uintptr_t(ehdr) + shdrs[ehdr->e_shstrndx].sh_offset);
+
+                    for (auto i = 0; i < ehdr->e_shnum; ++i) {
+                        const auto shdr = (ElfW(Shdr)*)(std::uintptr_t(shdrs) + i * ehdr->e_shentsize);
+
+                        if (std::strcmp(strTab + shdr->sh_name, ".text") != 0)
+                            continue;
+
+                        moduleInfo->base = (void*)(info->dlpi_addr + shdr->sh_offset);
+                        moduleInfo->size = shdr->sh_size;
+                        munmap(map, st.st_size);
+                        close(fd);
+                        return 1;
+                    }
+                    munmap(map, st.st_size);
+                }
+            }
+            close(fd);
+        }
+
+        moduleInfo->base = (void*)(info->dlpi_addr + info->dlpi_phdr[0].p_vaddr);
+        moduleInfo->size = info->dlpi_phdr[0].p_memsz;
+        return 1;
+    }, &moduleInfo);
+
+    return std::make_pair(moduleInfo.base, moduleInfo.size);
+#endif
+}
+
+[[nodiscard]] static auto generateBadCharTable(std::string_view pattern) noexcept
+{
+    assert(!pattern.empty());
+
+    std::array<std::size_t, (std::numeric_limits<std::uint8_t>::max)() + 1> table;
+
+    auto lastWildcard = pattern.rfind('?');
+    if (lastWildcard == std::string_view::npos)
+        lastWildcard = 0;
+
+    const auto defaultShift = (std::max)(std::size_t(1), pattern.length() - 1 - lastWildcard);
+    table.fill(defaultShift);
+
+    for (auto i = lastWildcard; i < pattern.length() - 1; ++i)
+        table[static_cast<std::uint8_t>(pattern[i])] = pattern.length() - 1 - i;
+
+    return table;
+}
+
+static std::uintptr_t findPattern(const char* moduleName, std::string_view pattern) noexcept
+{
+    static auto id = 0;
+    ++id;
+
+    const auto [moduleBase, moduleSize] = getModuleInformation(moduleName);
+
+    if (moduleBase && moduleSize) {
+        const auto lastIdx = pattern.length() - 1;
+        const auto badCharTable = generateBadCharTable(pattern);
+
+        auto start = static_cast<const char*>(moduleBase);
+        const auto end = start + moduleSize - pattern.length();
+
+        while (start <= end) {
+            int i = lastIdx;
+            while (i >= 0 && (pattern[i] == '?' || start[i] == pattern[i]))
+                --i;
+
+            if (i < 0)
+                return reinterpret_cast<std::uintptr_t>(start);
+
+            start += badCharTable[static_cast<std::uint8_t>(start[lastIdx])];
+        }
+    }
+#ifdef _WIN32
+    MessageBoxA(NULL, ("Failed to find pattern #" + std::to_string(id) + '!').c_str(), "risechanger", MB_OK | MB_ICONWARNING);
+#endif
+    return 0;
+}
+
+Memory::Memory() noexcept
+{
+    present = findPattern("gameoverlayrenderer", "\xFF\x15????\x8B\xF0\x85\xFF") + 2;
+    reset = findPattern("gameoverlayrenderer", "\xC7\x45?????\xFF\x15????\x8B\xD8") + 9;
+
+    clientMode = **reinterpret_cast<ClientMode***>((*reinterpret_cast<uintptr_t**>(interfaces->client))[10] + 5);
+    input = *reinterpret_cast<Input**>((*reinterpret_cast<uintptr_t**>(interfaces->client))[16] + 1);
+    globalVars = **reinterpret_cast<GlobalVars***>((*reinterpret_cast<uintptr_t**>(interfaces->client))[11] + 10);
+    disablePostProcessing = *reinterpret_cast<bool**>(findPattern(CLIENT_DLL, "\x83\xEC\x4C\x80\x3D") + 5);
+    loadSky = relativeToAbsolute<decltype(loadSky)>(findPattern(ENGINE_DLL, "\xE8????\x84\xC0\x74\x2D\xA1") + 1);
+    setClanTag = reinterpret_cast<decltype(setClanTag)>(findPattern(ENGINE_DLL, "\x53\x56\x57\x8B\xDA\x8B\xF9\xFF\x15"));
+    getSequenceActivity = reinterpret_cast<decltype(getSequenceActivity)>(findPattern(CLIENT_DLL, "\x55\x8B\xEC\x53\x8B\x5D\x08\x56\x8B\xF1\x83"));
+    auto temp = reinterpret_cast<std::uintptr_t*>(findPattern(CLIENT_DLL, "\xB9????\xE8????\x8B\x5D\x08") + 1);
+    hud = *temp;
+    findHudElement = relativeToAbsolute<decltype(findHudElement)>(reinterpret_cast<uintptr_t>(temp) + 5);
+    clearHudWeapon = reinterpret_cast<decltype(clearHudWeapon)>(findPattern(CLIENT_DLL, "\xE8????\x8B\xF0\xC6\x44\x24??\xC6\x44\x24"));
+    itemSystem = relativeToAbsolute<decltype(itemSystem)>(findPattern(CLIENT_DLL, "\xE8????\x0F\xB7\x0F") + 1);
+    viewRender = **reinterpret_cast<ViewRender***>(findPattern(CLIENT_DLL, "\x8B\x0D????\xFF\x75\x0C\x8B\x45\x08") + 2);
+    const auto tier0 = GetModuleHandleW(L"tier0");
+    equipWearable = reinterpret_cast<decltype(equipWearable)>(findPattern(CLIENT_DLL, "\x55\x8B\xEC\x83\xEC\x10\x53\x8B\x5D\x08\x57\x8B\xF9"));
+    keyValuesFromString = relativeToAbsolute<decltype(keyValuesFromString)>(findPattern(CLIENT_DLL, "\xE8????\x83\xC4\x04\x89\x45\xD8") + 1);
+    keyValuesFindKey = relativeToAbsolute<decltype(keyValuesFindKey)>(findPattern(CLIENT_DLL, "\xE8????\xF7\x45") + 1);
+    keyValuesSetString = relativeToAbsolute<decltype(keyValuesSetString)>(findPattern(CLIENT_DLL, "\xE8????\x89\x77\x38") + 1);
+    getPlayerViewmodelArmConfigForPlayerModel = relativeToAbsolute<decltype(getPlayerViewmodelArmConfigForPlayerModel)>(findPattern(CLIENT_DLL, "\xE8????\x89\x87????\x6A") + 1);
+    playerResource = *reinterpret_cast<PlayerResource***>(findPattern(CLIENT_DLL, "\x74\x30\x8B\x35????\x85\xF6") + 4);
+    setOrAddAttributeValueByNameFunction = relativeToAbsolute<decltype(setOrAddAttributeValueByNameFunction)>(findPattern(CLIENT_DLL, "\xE8????\x8B\x8D????\x85\xC9\x74\x10") + 1);
+    registeredPanoramaEvents = reinterpret_cast<decltype(registeredPanoramaEvents)>(*reinterpret_cast<std::uintptr_t*>(findPattern(CLIENT_DLL, "\xE8????\xA1????\xA8\x01\x75\x21") + 6) - 36);
+    makePanoramaSymbolFn = relativeToAbsolute<decltype(makePanoramaSymbolFn)>(findPattern(CLIENT_DLL, "\xE8????\x0F\xB7\x45\x0E\x8D\x4D\x0E") + 1);
+    moveData = **reinterpret_cast<MoveData***>(findPattern(CLIENT_DLL, "\xA1????\xF3\x0F\x59\xCD") + 1);
+    moveHelper = **reinterpret_cast<MoveHelper***>(findPattern(CLIENT_DLL, "\x8B\x0D????\x8B\x45?\x51\x8B\xD4\x89\x02\x8B\x01") + 2);
+    predictionRandomSeed = *reinterpret_cast<int**>(findPattern(CLIENT_DLL, "\x8B\x0D????\xBA????\xE8????\x83\xC4\x04") + 2);
+    getEventDescriptor = relativeToAbsolute<decltype(getEventDescriptor)>(findPattern(ENGINE_DLL, "\xE8????\x8B\xD8\x85\xDB\x75\x27") + 1);
+
+    localPlayer.init(*reinterpret_cast<Entity***>(findPattern(CLIENT_DLL, "\xA1????\x89\x45\xBC\x85\xC0") + 1));
+}
